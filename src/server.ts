148b268f332a24ad4e4e77f48b66253142c92e9a
Marton Borlay <borlaym@gmail.com>
2022-09-24 17:38:40 +0200

    feat(server): add basic server with all entries

---
+++ src/server.ts
@@ -0,0 +1,39 @@
+import express from "express"
+import fs from "fs"
+import path from "path"
+
+const app = express()
+const port = 3000
+
+app.get('/api/all', (req, res) => {
+  fs.readFile(path.join(__dirname, 'data.json'), 'utf-8', (err, data) => {
+    if (err) {
+      res.send({
+        error: {
+          code: 500,
+          message: err.message,
+        },
+        data: null
+      })
+    } else {
+      try {
+        let parsedData = JSON.parse(data)
+        res.send({
+          data: parsedData
+        })
+      } catch (err) {
+        res.send({
+          error: {
+            code: 500,
+            message: "Unable to read data",
+          },
+          data: null
+        })
+      }
+    }
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Backend listening on port ${port}`)
+})
