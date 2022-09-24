f36f267ed79252f4513cc865b4d10a96c8843dc8
Marton Borlay <borlaym@gmail.com>
2022-09-24 17:58:36 +0200

    feat(frontend): basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled'
+
+const RootContainer = styled.div`
+  padding: 50px 20px;
+  margin: 20px;
+  background: beige;
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RootContainer>
+      <p>Hello</p>
+    </RootContainer>
   );
 }
 
